16c445a1632b5af81cffa2b0d751119eef9d48be
unknown <maithilik2003@gmail.comgit config --global user.email maithilik2003@gmail.comgit config --global user.name maithilik281>
2025-06-30 01:00:53 +0530

    shared

--- icici/src/app/app.module.ts
+++ icici/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Dir1Directive } from './shared/dir1.directive';
+import { CustompipePipe } from './shared/custompipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Dir1Directive,
+    CustompipePipe
   ],
   imports: [
     BrowserModule,
